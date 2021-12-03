6eaf375eac4876aa1753827b5425bd37de87c9dc
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-12-02 21:01:27 -0300

    Java stdin and stdout I

---
+++ src/main/java/JavaStdinStdoutI.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class JavaStdinStdoutI {
+    /*
+     * Tarefa
+     *Neste desafio, você deve lerinteiros de stdin e depois imprimi-los em stdout.
+     *Cada número inteiro deve ser impresso em uma nova linha. Para tornar o problema um pouco mais fácil,
+     *uma parte do código é fornecida para você no editor a seguir.
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+
+
+}
